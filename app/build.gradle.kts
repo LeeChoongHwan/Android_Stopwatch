a1f8701957ac76afe4b90bd9b2d30dafc7d525c1
LeeChoongHwan <choo901@naver.com>
2024-06-05 22:27:02 +0900

    viewBinding setting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,9 @@ android {
     namespace = "com.choong.stopwatch"
     compileSdk = 33
 
+    viewBinding {
+        enable = true
+    }
     defaultConfig {
         applicationId = "com.choong.stopwatch"
         minSdk = 24
